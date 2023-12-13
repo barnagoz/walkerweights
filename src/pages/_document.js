1e3500484bd44a483c9219b3b387705da7d0e3f0
Barnabás Gőz <barnagoz@icloud.com>
2023-12-13 22:15:48 +0100

    feat: add shadcn ui and font

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { cn } from "@/lib/utils";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
+    <Html lang="hu">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito&display=optional"
+          rel="stylesheet"
+        />
+      </Head>
+      <body
+        className={cn("min-h-screen bg-background font-sans antialiased")}
+        style={{ fontFamily: "Nunito" }}
+      >
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
