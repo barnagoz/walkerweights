427eb2df594a902f90341e1f8e36c96f12a8bceb
Barnabás Gőz <barnagoz@icloud.com>
2023-12-14 15:12:40 +0100

    feat: add next-auth

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+
+export const authOptions = {
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+};
+export default NextAuth(authOptions);
