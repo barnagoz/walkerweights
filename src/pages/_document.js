5e0fb2665959e771848fb26acfe363f89103d1a0
Barnabás Gőz <barnagoz@icloud.com>
2024-01-03 15:02:00 +0100

    feat: add meta info

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,39 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="hu">
-      <Head></Head>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#bd9e00" />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Walker &amp; Weights"
+        />
+        <meta name="application-name" content="Walker &amp; Weights" />
+        <meta name="msapplication-TileColor" content="#2d89ef" />
+        <meta name="theme-color" content="#ffffff" />
+        <title>Walker &amp; Weights</title>
+        <meta name="og:title" content="Walker &amp; Weights" />
+        <meta name="og:image" content="/og.png" />
+        <meta name="og:url" content="https://walkerweights.barnagoz.com" />
+        <meta name="author" content="Barnabás Gőz" />
+      </Head>
       <body className={cn("min-h-screen bg-background font-sans antialiased")}>
         <Main />
         <NextScript />
