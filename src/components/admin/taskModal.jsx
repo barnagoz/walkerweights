5f8c1abc9ce72fde69cd99c3986842134bcd0b3f
Barnabás Gőz <barnagoz@icloud.com>
2024-11-26 23:11:41 +0100

    fix: taskmodal crashes certain cases

--- src/components/admin/taskModal.jsx
+++ src/components/admin/taskModal.jsx
@@ -10,10 +10,10 @@ import {
 } from "@/components/ui/dialog";
 import { Separator } from "@/components/ui/separator";
 import { Textarea } from "@/components/ui/textarea";
+import axios from "axios";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useState } from "react";
-import axios from "axios";
 import { toast } from "sonner";
 
 export default function TaskModal ({task, children, getData}) {
@@ -63,30 +63,43 @@ export default function TaskModal ({task, children, getData}) {
 					<DialogTitle><Badge className={"mr-2"}>{task.status}</Badge>{task.title}</DialogTitle>
 					<DialogDescription>{task.description}</DialogDescription>
 				</DialogHeader>
-				{task.type === "form" && (
-					<h2>Forms coming soon!</h2>
-				)}
-				{task.type === "pdf" && (
-					<div>
-						<Link href={task.file_submission?.file_url}><Button>Fájl megnyitása→</Button></Link>
-						<p className={"text-muted muted mt-2"}>Beküldve
-							ekkor: {new Date(task.file_submission?.submitted_at).toLocaleString()}</p>
+				{task.status === "Új" && (
+					<div className={"flex w-full p-4 justify-center items-center"}>
+						<p>Várakozás beküldésre...</p>
 					</div>
 				)}
-				{task.type === "xlsx" && (
-					<div>
-						<Link href={task.file_submission?.file_url}><Button>Fájl letöltése→</Button></Link>
-						<p className={"text-muted muted mt-2"}>Beküldve
-							ekkor: {new Date(task.file_submission?.submitted_at).toLocaleString()}</p>
-					</div>
+				{task.status !== "Új" && (
+					<>
+						{task.type === "form" && (
+							<h2>Forms coming soon!</h2>
+						)}
+						{task.type === "pdf" && (
+							<div>
+								<Link href={task.file_submission?.file_url}><Button>Fájl megnyitása→</Button></Link>
+								<p className={"text-muted muted mt-2"}>Beküldve
+									ekkor: {new Date(task.file_submission?.submitted_at).toLocaleString()}</p>
+							</div>
+						)}
+						{task.type === "xlsx" && (
+							<div>
+								<Link href={task.file_submission?.file_url}><Button>Fájl letöltése→</Button></Link>
+								<p className={"text-muted muted mt-2"}>Beküldve
+									ekkor: {new Date(task.file_submission?.submitted_at).toLocaleString()}</p>
+							</div>
+						)}
+					</>
 				)}
-				<Separator/>
-				<Textarea value={Comment} onChange={(e) => setComment(e.target.value)} id={"comment"}
-				          placeholder={"Megjegyzés"}></Textarea>
-				<div className={"flex w-full gap-2"}>
-					<Button variant={"destructive"} className={"w-full"} onClick={resend}>Újraküldendő</Button>
-					<Button className={"w-full"} onClick={accept}>Elfogadás</Button>
-				</div>
+				{task.status === "Beküldve" && (
+					<>
+						<Separator/>
+						<Textarea value={Comment} onChange={(e) => setComment(e.target.value)} id={"comment"}
+						          placeholder={"Megjegyzés"}></Textarea>
+						<div className={"flex w-full gap-2"}>
+							<Button variant={"destructive"} className={"w-full"} onClick={resend}>Újraküldendő</Button>
+							<Button className={"w-full"} onClick={accept}>Elfogadás</Button>
+						</div>
+					</>
+				)}
 			</DialogContent>
 		</Dialog>
 	)
